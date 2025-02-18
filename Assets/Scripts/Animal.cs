d61e1c3a1993258b0d4ad27601c9ade8f47fbfc8
Caldo-pixel <200602048@student.georgianc.on.ca>
2025-02-18 14:24:57 -0500

    added Animal script with some common attributes and behaviours

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Animal
+{
+    public int age;
+    public float speed;
+    public float weight;
+    public float height;
+    public float hunger;
+    public float energy;
+    public Color color;
+
+    public void Eat()
+    {
+        hunger = 0.0f;
+    }
+
+    public void Exercise()
+    {
+
+    }
+
+    public void Sleep()
+    {
+
+    }
+
+
+}
